cdbb1bce3c73d99935144987ad7c43fc7ab450f8
tienZheng <zhengcheng@tien.local>
2019-02-27 10:24:19 +0800

    补充了严格及参数数量验证两个规则

--- src/Core.php
+++ src/Core.php
@@ -10,9 +10,28 @@ namespace Tien\ThinkSwagger;
 
 
 use Tien\ThinkSwagger\exceptions\Exception;
+use Tien\ThinkSwagger\exceptions\TypeStringException;
 
 trait Core
 {
+
+    /**
+     * tien_max_3   //最多 3 个参数
+     * tien_min_2   //最小 2 个必须参数
+     * tien_strict  //严格检验，不在列表中的不能存在
+     * @var array
+     */
+    protected $tienMax          = 'tien_max_';
+
+    protected $tienMaxNum       = 0;
+
+    protected $tienMinNum       = 0;
+
+    protected $tienStrictBool   = false;
+
+    protected $tienMin          = 'tien_min_';
+
+    protected $tienStrict       = 'tien_strict';
 
     /**
      * 翻译
@@ -27,6 +46,11 @@ trait Core
      * @var array
      */
     protected $ruleKey = [];
+
+    /**
+     * @var array
+     */
+    protected $checkRule = [];
 
     /**
      * :获取规则
@@ -77,6 +101,7 @@ trait Core
             //明面上存在该规则
             if (in_array($key, $this->ruleKey)) {
                 $splitKey = $this->splitRuleKey($key, $this->translate[$key]);
+                $this->checkRule[] = $key;
                 $searchRule[$splitKey] = $rule[$splitKey];
                 continue;
             }
@@ -85,13 +110,127 @@ trait Core
             $realKey = $this->removeSupply($key);
             if (in_array($realKey, $this->ruleKey)) {
                 $splitKey = $this->splitRuleKey($realKey, $this->translate[$realKey]);
+                $this->checkRule[] = $realKey;
                 $searchRule[$splitKey] = $this->removeRequire($rule[$splitKey]);
-            } else {
+            } elseif(!$this->handleInSpecialKeys($realKey)) {
+                //还是不存在规则中，再排除可能的严格规则等
                 throw new Exception('不存在该变量的验证规则：' . $key);
             }
         }
         return $searchRule;
     }
+
+    public function getCheckRule()
+    {
+        return $this->checkRule;
+    }
+
+    /**
+     * :
+     *
+     * @param $str
+     * @throws TypeStringException
+     */
+    public function setTienMax($str)
+    {
+        if (!is_string($str)) {
+            throw new TypeStringException('$str 应该是字符串');
+        }
+        $this->tienMax = $str;
+    }
+
+    /**
+     * :
+     *
+     * @param $str
+     * @throws TypeStringException
+     */
+    public function setTienMin($str)
+    {
+        if (!is_string($str)) {
+            throw new TypeStringException('$str 应该是字符串');
+        }
+        $this->tienMin = $str;
+    }
+
+    public function getTienMaxNum()
+    {
+        return $this->tienMaxNum;
+    }
+
+
+    public function getTienMinNum()
+    {
+        return $this->tienMinNum;
+    }
+
+    public function getTiemStrictBool()
+    {
+        return $this->tienStrictBool;
+    }
+
+    /**
+     * :
+     *
+     * @param $strict
+     * @throws TypeStringException
+     */
+    public function setTienStrict($strict)
+    {
+        if (!is_string($strict)) {
+            throw new TypeStringException('$strict 应该是字符串');
+        }
+        $this->tienStrict = $strict;
+    }
+
+    /**
+     * :处理特殊字符串
+     *
+     * @param $key
+     * @return bool
+     */
+    protected function handleInSpecialKeys($key)
+    {
+        //是否是否是严格字符串
+        if ($this->tienStrict == $key) {
+            $this->tienStrictBool = true;
+            return true;
+        }
+
+        //验证是否是限制数量最大的
+        if (strpos($key, $this->tienMax) === 0) {
+            //获取数量
+            $num = (int)$this->getTienNum($key, $this->tienMax);
+            if (!$num) {
+                return false;
+            }
+            $this->tienMaxNum = $num;
+            return true;
+        }
+
+        //验证是否是限制数量最小的
+        if (strpos($key, $this->tienMin) === 0) {
+            //获取数量
+            $num = (int)$this->getTienNum($key, $this->tienMin);
+            $this->tienMinNum = $num;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * :
+     *
+     * @param $str
+     * @param $tienStr
+     * @return bool|string
+     */
+    protected function getTienNum($str, $tienStr)
+    {
+        return substr($str, strlen($tienStr));
+    }
+
+
 
     /**
      * :
---
+++ src/exceptions/TypeStringException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tien
+ * Date: 2019/2/27
+ * Time: 10:21 AM
+ */
+
+namespace Tien\ThinkSwagger\exceptions;
+
+
+class TypeStringException extends Exception
+{
+
+}
